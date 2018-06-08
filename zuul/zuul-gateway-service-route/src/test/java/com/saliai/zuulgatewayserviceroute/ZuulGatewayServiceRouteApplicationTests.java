e7b48b6478e8362a9b0a5140420630b859b84b29
se7en-11 <76441943@qq.com>
2018-06-08 16:54:38 +0800

    服务网关 - 服务路由配置

---
+++ zuul/zuul-gateway-service-route/src/test/java/com/saliai/zuulgatewayserviceroute/ZuulGatewayServiceRouteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saliai.zuulgatewayserviceroute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulGatewayServiceRouteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
